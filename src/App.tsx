b25106a410a152d513626380ef5376f5885f0566
BelousovaVK <belousova.vitalina@gmail.com>
2023-04-20 20:46:06 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { useState } from "react";
+// 07a92f5fb756a201a6c5d7822a16965b - APIkey
+// lat = 57.62987 lon = 39.87368
+function App(): JSX.Element {
+  const [cityInfo, setCityInfo] = useState<string>("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={(e) => setCityInfo(e.target.value)}></input>
+      <button
+        onClick={() => {
+          fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInfo.split(" ")[0]},${cityInfo.split(" ")[1]}&appid=07a92f5fb756a201a6c5d7822a16965b`)
+            .then((file) => file.json())
+            .then((info) => console.log(info));
+        }}
+      ></button>
+      <p></p>
     </div>
   );
 }
